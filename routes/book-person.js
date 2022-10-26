956fd09dc8168d3b26011d25a5b8a0f755de9c74
Julio Cesar <juliocesardantss@gmail.com>
2022-10-26 16:13:05 -0300

    exercicios de TDD

--- routes/book-person.js
+++ routes/book-person.js
@@ -1,4 +1,15 @@
-const persona = []
+let persona = []
+
+const getPersonas = () => {
+    return persona
+}
+const getById = (id) => {
+    const foundPersons = persona.find(person => person.id === Number(id))
+    const verificando = foundPersons == undefined ?
+        "Nao encontrada, verifique se esta correto"
+        : foundPersons
+    return verificando
+}
 const cadastrandoPerson = (id, name, dataNasc) => {
     const infoPerson = {
         id: id,
@@ -11,4 +22,72 @@ const cadastrandoPerson = (id, name, dataNasc) => {
     persona.push(infoPerson)
     return persona
 }
-module.exports = cadastrandoPerson
+
+const removePerssoa = (id) => {
+    persona = persona.filter(person => person.id != id)
+}
+
+const putByPersona = (id, nome, dataNasc) => {
+    let getById = persona.find(persona => persona.id === Number(id))
+    const verification = getById == undefined ?
+        "Nao encontrado"
+        : getById
+    verification.id = id
+    verification.name = nome
+    verification.dataNasc = dataNasc
+}
+
+const getPessoasMaiorDeIdade = () => {
+    let pessoasMaiorDeIdade = []
+    for (let i = 0; i < persona.length; i++) {
+        const pessoa = persona[i];
+        if (getAge(pessoa.dataNasc) >= 18) {
+            pessoasMaiorDeIdade.push(pessoa)
+        }
+    }
+    return pessoasMaiorDeIdade
+}
+
+module.exports = {
+    getById,
+    cadastrandoPerson,
+    getPersonas,
+    removePerssoa,
+    putByPersona,
+    getPessoasMaiorDeIdade
+}
+
+function getAge(dateString) {
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
+
+
+
+// const personaMaiorDeIdade = (id) => {
+//     const personaMaior = myVar.map(persona => persona.data.split("/"))
+//     const result = personaMaior.map(num => 2022 - num[2])
+//     console.log(result)
+// }
+// personaMaiorDeIdade()
+// const myVar = [
+//     { id: 1, nome: "julio", data: "19/05/1990" },
+//     { nome: "cesar", data: "23/08/1990" },
+//     { nome: "renato", data: "31/03/2030" },
+//     { nome: "raul", data: "03/01/2040" },
+//     { nome: "fulano", data: "08/09/2010" },
+//     { nome: "cicrano", data: "28/11/2020" },
+//     { nome: "chico", data: "31/01/1980" },
+//     { nome: "tio", data: "13/11/1980" },
+// ]
+// const sortName = myVar.map(nome => nome.nome).sort()
+// const func = myVar.map(nome => nome.data.split("/")).sort()
+// const map2 = func.map(numb => numb[1]).sort()
+// console.log(sortName)
+// console.log(map2)
--- routes/book-person.test.js
+++ routes/book-person.test.js
@@ -1,7 +1,50 @@
-const cadastrandoPerson = require("./book-person")
-test("Cadastrando person", () => {
-    expect(cadastrandoPerson(1, "Julio", "19/05/1990")).toEqual([{ id: 1, name: "Julio", dataNasc: "19/05/1990" }])
+const book = require("./book-person")
+const getById = book.getById
+const cadastrandoPerson = book.cadastrandoPerson
+const getPersonas = book.getPersonas
+const removePerssoa = book.removePerssoa
+const putByPersona = book.putByPersona
+const getPessoasMaiorDeIdade = book.getPessoasMaiorDeIdade
+test("Deve cadastrar pessoas", () => {
+    expect(cadastrandoPerson(1, "Julio", "2022-05-10")).toEqual([{ id: 1, name: "Julio", dataNasc: "2022-05-10" }])
 })
-test("erro de cadastro", () => {
+test("Deve retorna uma menssagem de erro quando os valores forem invalidos", () => {
     expect(cadastrandoPerson(" ", " ", " ")).toEqual("Todos os Campos sao obrigatorios")
-}) 
+})
+test("Deve retornar a pessoa pesquisada pelo ID", () => {
+    cadastrandoPerson(2, "Cesar", "2022-05-10")
+    expect(getById(2)).toEqual({ id: 2, name: "Cesar", dataNasc: "2022-05-10" })
+})
+test("Deve retornar menssagem de nao encontrada", () => {
+    expect(getById(3)).toEqual("Nao encontrada, verifique se esta correto")
+})
+test("Deve retornar todas as pessoas cadastrada", () => {
+    cadastrandoPerson(3, "Maria", "2022-05-10")
+    expect(getPersonas()).toEqual([
+        { id: 1, name: "Julio", dataNasc: "2022-05-10" },
+        { id: 2, name: "Cesar", dataNasc: "2022-05-10" },
+        { id: 3, name: "Maria", dataNasc: "2022-05-10" }
+    ])
+})
+test("Deve excluir uma pessoa", () => {
+    cadastrandoPerson(5, "Rita", "2022-05-10")
+    removePerssoa(5)
+    expect(getById(5)).toEqual("Nao encontrada, verifique se esta correto")
+})
+test("Deve fazer Alteracoes em uma pessoa", () => {
+    putByPersona(1, "Snow", "Sem data")
+    expect(getById(1)).toEqual({ dataNasc: "Sem data", id: 1, name: "Snow" })
+})
+test("Deve retornar pessoa maior de idade", () => {
+    cadastrandoPerson(6, "Luana", "2022-05-10")
+    cadastrandoPerson(7, "Paula", "1980-05-10")
+    expect(getPessoasMaiorDeIdade()).toEqual(          // 1
+        expect.arrayContaining([      // 2
+            expect.objectContaining({   // 3
+                id: 7,
+                name: 'Paula',
+                dataNasc: "1980-05-10"                // 4
+            })
+        ])
+    )
+})
